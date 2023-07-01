47c0c55505dcd651f2a2e45eca8d70cf2e0647cc
Alexander <Alexander29_90@icloud.com>
2023-07-01 23:22:31 +0500

    Initial Commit

---
+++ I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Александр on 01.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
